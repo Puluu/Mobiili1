ae07b74a31a4d2aa39610599ce27b7517523709f
Puluu <jani.putkonen@edu.savonia.fi>
2020-01-08 15:46:31 +0200

    versio 1

---
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.shoppinglist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textarvo;
+    private Button button_save;
+    private Button button_done;
+    private EditText editText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.textarvo = findViewById(R.id.textView);
+        this.button_save = findViewById(R.id.button3);
+        this.button_done = findViewById(R.id.button4);
+        this.editText2 = findViewById(R.id.editText);
+
+        this.button_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(editText2.getText().length()<= 3 || editText2.getText().length() > 15){
+                    Context context = getApplicationContext();
+                    CharSequence text = "Liian lyhyt tai pitkä teksti!";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
+
+                }
+            }
+        });
+
+    }
+}
