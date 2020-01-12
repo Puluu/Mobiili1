b520dc4342adb1325700c6336e508f680e509031
Puluu <jani.putkonen@edu.savonia.fi>
2020-01-12 18:40:29 +0200

    versio 2

--- app/src/main/java/com/example/shoppinglist/Main2Activity.java
+++ app/src/main/java/com/example/shoppinglist/Main2Activity.java
@@ -2,13 +2,36 @@ package com.example.shoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private Button button_back;
+    private TextView textarvo2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        this.button_back = findViewById(R.id.button);
+        this.textarvo2 = findViewById(R.id.textView2);
+
+        Intent myIntent = getIntent();
+        String items = myIntent.getStringExtra("items");
+        textarvo2.setText(items);
+
+
+        this.button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.shoppinglist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         this.button_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, Main2Activity.class);
+                myIntent.putExtra("items", editText2.getText());
+
                 if(editText2.getText().length()<= 3 || editText2.getText().length() > 15){
                     Context context = getApplicationContext();
                     CharSequence text = "Liian lyhyt tai pitkä teksti!";
@@ -39,8 +43,25 @@ public class MainActivity extends AppCompatActivity {
                     Toast toast = Toast.makeText(context, text, duration);
                     toast.show();
 
+                }
+                else{
+                    Context context = getApplicationContext();
+                    CharSequence text = "Teksti tallennettu listaan";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();
+
 
                 }
+            }
+        });
+
+        this.button_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(myIntent);
             }
         });
 
